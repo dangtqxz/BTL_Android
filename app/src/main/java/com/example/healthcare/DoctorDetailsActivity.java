2e95a229448195ac64a11875f59ec47cd352426e
Lam Dang <nguyenhuulamdang@gmail.com>
2024-04-11 14:46:01 +0700

    Doctor Details Activity

---
+++ app/src/main/java/com/example/healthcare/DoctorDetailsActivity.java
@@ -0,0 +1,124 @@
+package com.example.healthcare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DoctorDetailsActivity extends AppCompatActivity {
+
+    private String[][] doctor_details1 = {
+            {"Doctor Name: John Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "600"},
+            {"Doctor Name: Jane Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "900"},
+            {"Doctor Name: John Smith", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "300"},
+            {"Doctor Name: Jane Smith", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "500"},
+            {"Doctor Name: John Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "800"}
+    };
+
+    private String[][] doctor_details2 = {
+            {"Doctor Name: John Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "600"},
+            {"Doctor Name: Jane Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "900"},
+            {"Doctor Name: John Smith", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "300"},
+            {"Doctor Name: Jane Smith", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "500"},
+            {"Doctor Name: John Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "800"}
+    };
+
+    private String[][] doctor_details3 = {
+            {"Doctor Name: John Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "600"},
+            {"Doctor Name: Jane Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "900"},
+            {"Doctor Name: John Smith", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "300"},
+            {"Doctor Name: Jane Smith", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "500"},
+            {"Doctor Name: John Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "800"}
+    };
+
+    private String[][] doctor_details4 = {
+            {"Doctor Name: John Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "600"},
+            {"Doctor Name: Jane Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "900"},
+            {"Doctor Name: John Smith", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "300"},
+            {"Doctor Name: Jane Smith", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "500"},
+            {"Doctor Name: John Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "800"}
+    };
+
+    private String[][] doctor_details5 = {
+            {"Doctor Name: John Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "600"},
+            {"Doctor Name: Jane Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "900"},
+            {"Doctor Name: John Smith", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "300"},
+            {"Doctor Name: Jane Smith", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "500"},
+            {"Doctor Name: John Doe", "Hospital Address: HaNoi", "Exp: 5yrs", "Mobile No: 0123456789", "800"}
+    };
+
+    TextView tv;
+    Button btn;
+    String[][] doctor_details = {};
+    ArrayList list;
+    HashMap<String, String> item;
+    SimpleAdapter sa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_doctor_details);
+
+        tv = findViewById(R.id.textViewDDTitle);
+        btn = findViewById(R.id.buttonDDBack);
+
+        Intent it = getIntent();
+        String title = it.getStringExtra("title");
+        tv.setText(title);
+
+        if (title.compareTo("Family Physicians")==0) {
+            doctor_details = doctor_details1;
+        } else if (title.compareTo("Dietician")==0) {
+            doctor_details = doctor_details2;
+        } else if (title.compareTo("Dentist")==0) {
+            doctor_details = doctor_details3;
+        } else if (title.compareTo("Surgeon")==0) {
+            doctor_details = doctor_details4;
+        } else if (title.compareTo("Cardiologists")==0) {
+            doctor_details = doctor_details5;
+        }
+
+        list = new ArrayList();
+        for (int i=0; i<doctor_details.length; i++) {
+            item = new HashMap<String, String>();
+            item.put("line1", doctor_details[i][0]);
+            item.put("line2", doctor_details[i][1]);
+            item.put("line3", doctor_details[i][2]);
+            item.put("line4", doctor_details[i][3]);
+            item.put("line5", "Consultation Fee: " + doctor_details[i][4]+"/-");
+            list.add(item);
+        }
+        sa = new SimpleAdapter(this, list,
+                R.layout.multi_lines, new String[]{"line1", "line2", "line3", "line4", "line5"},
+                new int[]{R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e});;
+        ListView lv = findViewById(R.id.listViewDD);
+        lv.setAdapter(sa);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DoctorDetailsActivity.this, FindDoctorActivity.class));
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/example/healthcare/FindDoctorActivity.java
+++ app/src/main/java/com/example/healthcare/FindDoctorActivity.java
@@ -12,14 +12,65 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class FindDoctorActivity extends AppCompatActivity {
-    CardView exit;
+    CardView familyphisician, dietician, dentist, surgeon, cardiologists,exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_find_doctor);
 
+        familyphisician = findViewById(R.id.cardFDFamilyPhysician);
+        dietician = findViewById(R.id.cardFDDietician);
+        dentist = findViewById(R.id.cardFDDentits);
+        surgeon = findViewById(R.id.cardFDSurgeon);
+        cardiologists = findViewById(R.id.cardFDCardiologists);
         exit = findViewById(R.id.cardFDBack);
+
+        familyphisician.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Family Physicians");
+                startActivity(it);
+            }
+        });
+
+        dietician.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Dietician");
+                startActivity(it);
+            }
+        });
+
+        dentist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Dentist");
+                startActivity(it);
+            }
+        });
+
+        surgeon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Surgeon");
+                startActivity(it);
+            }
+        });
+
+        cardiologists.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Cardiologists");
+                startActivity(it);
+            }
+        });
+
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
